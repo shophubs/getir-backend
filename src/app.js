4f0161dd22633a3d7c50a0896ff6226dbd27acd8
Anurag Vishwa <anuragvishwacs@gmail.com>
2023-12-02 15:39:21 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ require('./db/mongoose')
 const recordRouter = require('./routers/record')
 
 const app = express()
-console.log("vishwa");
+console.log("vishwas");
 app.use(express.json())
 app.use(recordRouter)
 module.exports = app
